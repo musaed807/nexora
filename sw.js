8fb6837b905eb4891a331c3ac85e461157961d42
musaed807 <msad5372@gmail.com>
2026-02-15 22:31:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open('nexora-v1').then(c=>c.addAll(['./']))
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
